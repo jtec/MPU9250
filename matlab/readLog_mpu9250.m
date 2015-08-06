242bd8cfe80c3cd009640ec57c987d1dd1407c36
Jan <j.bolting@gmx.de>
2015-08-06 15:40:14 +0200

    Fixed decoding script quick%dirty, if data alignment is important, better use some proper message format.

--- matlab/readLog_mpu9250.m
+++ matlab/readLog_mpu9250.m
@@ -2,18 +2,24 @@ function data = readLog_mpu9250(filename)
 disp([mfilename '>> Reading file ' filename ' ...'])
 % Open file
 fid = fopen(filename);
-% Read file as a whole:
+% Read file line by line:
 d = textscan(fid,'%f %f %f %f %f %f %f %f',...
-'Delimiter',',');
-% Ignore first and last lines, as they may be corrupted:
-data.time_s = 1e-6 * d{1}(2:end-1);
-data.acc_mps2 = [d{2}(2:end-1) ...
-                 d{3}(2:end-1) ...
-                 d{4}(2:end-1)];
-data.gyr_degps = [d{5}(2:end-1) ...
-                  d{6}(2:end-1) ...
-                  d{7}(2:end-1)];
-data.temp_degC = d{8}(2:end-1);
+'Delimiter',',', 'EmptyValue',0);
+% Ignore first and last lines, as they may be corrupted.
+maxl = length(d{1});
+for k=1:length(d)
+    if maxl > length(d{k})
+       maxl = length(d{k});
+    end
+end
+data.time_s = 1e-6 * d{1}(2:maxl-1);
+data.acc_mps2 = [d{2}(2:maxl-1) ...
+                 d{3}(2:maxl-1) ...
+                 d{4}(2:maxl-1)];
+data.gyr_degps = [d{5}(2:maxl-1) ...
+                  d{6}(2:maxl-1) ...
+                  d{7}(2:maxl-1)];
+data.temp_degC = d{8}(2:maxl-1);
              
 % Close file after data extraction:
 fclose(fid);
